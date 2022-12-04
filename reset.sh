4f5c0460330b1c0d40021c672add4f549447b27a
nathanbellis <nathanbellis@gmail.com>
2022-12-04 15:34:16 -0600

    Originally smreset.sh

---
+++ reset.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rm -r fqtctemp
+rm -r ca3temp
+rm -r ca5temp
+rm -r counts
+rm -r htseq
+rm -r rDNAcounts
+rm -r mapped_reads
+rm -r sorted_reads
+rm -r bt2logs
+rm -r fastqcreports
+rm -r tables
+rm smtest.err
+rm smtest.out
